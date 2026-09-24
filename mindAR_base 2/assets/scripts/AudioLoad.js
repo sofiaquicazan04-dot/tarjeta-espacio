121a4521909e57d46ef9f1a894fc49dd7afc9cc3
sofiaquicazan04-dot <sofia.quicazan04@gmail.com>
2026-09-24 13:30:48 -0500

    Delete mindAR_base 2 directory

--- mindAR_base 2/assets/scripts/AudioLoad.js
+++
@@ -1,5 +0,0 @@
-const clickSound = player.loadAudio('click', 'https://it-dun-pi.vercel.app/assets/audio/select.mp3', (audio) => {
-    audio.setLoop(false);
-    audio.setVolume(1);
-    // audio.play();
-});
